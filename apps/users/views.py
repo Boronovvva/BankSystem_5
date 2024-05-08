31dd2bcf086d45842edb92c30dcc5d001cd6b72d
Boronovvva <saraboronova003@gmail.com>
2024-05-08 13:14:02 +0600

    updeate

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from apps.users.models import User
+from apps.users.serializers import UserSerializer,UserRegisterSerializer
 
-# Create your views here.
+
+class UserAPI(GenericViewSet,
+                   mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
